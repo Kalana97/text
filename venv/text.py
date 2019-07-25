022d4650f848b24d079e23f154e3e13065f5d596
Kalana Bandara <44183583+Kalana97@users.noreply.github.com>
2019-07-25 11:41:17 +0530

    Update text.py

--- venv/text.py
+++ venv/text.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome("text/Driver/chromedriver.exe")
+browser = webdriver.Chrome("Driver/chromedriver.exe")
 browser.get('https://phoenix.vetstoria.com/login#!/')
